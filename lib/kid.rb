bc78678c2f41aa09c2566817632cc602d7065913
Ted Sazon <tedsazon@gmail.com>
2019-01-09 17:44:51 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,11 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 class Kid
+  attr_accessor :name
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  include Dance
+  def initialize(name)
+    @name = name
+  end
 end
